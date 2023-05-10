1b80dd6000d7eed2369076b13287d5565576d554
huytm2k3 <huytm2k3@gmail.com>
2023-05-11 03:39:46 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <main>
+      <header>
+        <img src="./assets/pinia-logo.svg" alt="">
+        <h2>Pinia tasks</h2>
+      </header>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
